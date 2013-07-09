608f8e39955b828f33f03703edaebf148fa3c16b
Zoltan Orban <zoltan.orban@gmail.com>
2013-07-09 12:53:48 +0200

    Update web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf  = fs.fs.readFileSync('index.html');
+  var buf  = fs.readFileSync('index.html');
   response.send(buf.toString()); 
 });
 
