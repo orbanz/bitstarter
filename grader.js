1f0ce6cde7aa570ab6de67e071831ddfa446e442
Zoltan Orban <zoltan.orban@gmail.com>
2013-07-10 07:45:21 +0000

    bugfix

--- grader.js
+++ grader.js
@@ -24,7 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('./restler');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 var URL_DEFAULT="None";
