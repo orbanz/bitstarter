387b2bfe4d8baa04fb8c41f970d921da73e8a667
Zoltan Orban <zoltan.orban@gmail.com>
2013-07-10 09:24:44 +0200

    Update grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT="None";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +36,11 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(url) {
+ var urlstr = url.toString();
+ return urlstr;
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,8 +71,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'HTTP URL', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson = "";
+    if(program.url && program.url != URL_DEFAULT) {
+     console.log(program.url); 
+    } else {
+     checkJson = checkHtmlFile(program.file, program.checks); 
+    }
+    
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
